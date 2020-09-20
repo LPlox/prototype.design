b4eb7598328ba9d827dc546ba2a80f4512e5554d
LPlox <lina.js.wang@gmail.com>
2020-09-20 14:39:25 +0200

    new layout in  font display

--- src/components/FontDisplay.js
+++ src/components/FontDisplay.js
@@ -22,33 +22,41 @@ function FontDisplay({ font, divStyle }) {
 
   return (
     <div className="font-display" style={divStyle}>
+      <p className="font__header-type">{font.header.font}</p>
+      <p className="font__subheader-type">{font.subheader.font}</p>
+      <p className="font__body-type">{font.body.font}</p>
       <h1
+        className="font__header"
         style={{
           fontFamily: font.header.font,
           fontSize: "2.4rem",
           fontWeight: font.header.weight,
         }}
       >
-        {font.header.font}
+        Title
       </h1>
       <h2
+        className="font__subheader"
         style={{
           fontFamily: font.subheader.font,
-          fontSize: "1.6rem",
+          fontSize: "1.2rem",
           fontWeight: font.subheader.weight,
         }}
       >
-        {font.subheader.font}
+        Sub header
       </h2>
       <p
+        className="font__body"
         style={{
           fontFamily: font.body.font,
-          fontSize: "1rem",
+          fontSize: "0.8rem",
           fontWeight: font.body.weight,
         }}
       >
-        {font.body.font +
-          " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat
       </p>
     </div>
   );
